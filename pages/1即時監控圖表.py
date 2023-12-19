b6467b826a43b1b6d15a88f1f2bd81ab8f355948
ZhengFuLiu <yyy1021tw@gmail.com>
2023-12-19 16:57:17 +0800

    renew

--- pages/1即時監控圖表.py
+++ pages/1即時監控圖表.py
@@ -1,6 +1,326 @@
 import streamlit as st
+import pandas as pd
 
-with st.sidebar:
-    st.markdown('''掌握客戶端與公司設備情形，並顯示電池各項指標，即時排除設備異常。''')
+def create_data_block(title, value, background_color):
+    """創建一個數據展示塊。"""
+    return f"""
+        <div style='background-color: {background_color}; padding: 10px; border-radius: 5px; text-align: center;'>
+            <p style='color: black; font-size: 32px; margin: 0;'>{title}</p>
+            <p style='color: black; font-size: 40px; margin: 0;'>{value}</p>
+        </div>
+        """
 
-st.markdown('# 即時監測圖表')
+def main_content():
+    st.title("即時監測圖表")
+    st.markdown("#")
+
+    # Data blocks
+    t1, t2, t3, t4, t5 = st.columns(5)
+    t1.markdown(create_data_block("總數", "4", "#636EFA"), unsafe_allow_html=True)
+    t2.markdown(create_data_block("運行中", "2", "#34A853"), unsafe_allow_html=True)
+    t3.markdown(create_data_block("停止", "0", "#FECB52"), unsafe_allow_html=True)
+    t4.markdown(create_data_block("關機", "2", "#7F7F7F"), unsafe_allow_html=True)
+    t5.markdown(create_data_block("異常", "0", "#EF553B"), unsafe_allow_html=True)
+
+    # Main interactive components
+    st.markdown("#")
+    st.markdown(f"""
+        <div style='background-color: #19D3F3; padding: 10px; border-radius: 5px;'>
+            <p style='color: black; font-size: 20px; margin: 0;'>查詢條件</p>
+        </div>
+        """, unsafe_allow_html=True)
+    col1, col2 = st.columns(2)
+
+    with col1:
+        option = st.selectbox('工廠', ('總公司', '客戶服務'), index=None)
+
+    if option == "總公司":
+        department_options = ('業務部',)
+    elif option == "客戶服務":
+        department_options = ("客戶100",)
+    else:
+        department_options = ("",)
+        disabled = True
+
+    # 顯示部門選項
+    with col2:
+        if 'disabled' in locals():  # 檢查是否需要禁用選項
+            suboption = st.selectbox("部門", department_options, disabled=True)
+        else:
+            suboption = st.selectbox("部門", department_options)
+
+    cb1, cb2, cb3, cb4 = st.columns(4)
+    on = cb1.checkbox('運行中', value=True)
+    stop = cb2.checkbox('停止', value=True)
+    off = cb3.checkbox('關機', value=True)
+    ng = cb4.checkbox('異常', value=True)
+
+    st.markdown("#")
+    st.markdown(f"""
+        <div style='background-color: #19D3F3; padding: 10px; border-radius: 5px;'>
+            <p style='color: black; font-size: 20px; margin: 0;'>{option}{''.join(department_options)}</p>
+        </div>
+        """, unsafe_allow_html=True)
+
+    if (f"{option}{''.join(department_options)}" == "總公司業務部") & (on):
+        card1, card2 = st.columns(2, gap="small")
+        with card1:
+            st.markdown("""
+                <div style='background-color: #34A853; padding: 10px; border-radius: 5px 5px 0 0; text-align: center;'>
+                    <p style='color: white; font-size: 32px; margin: 0;'>堆高機003</p>
+                </div>
+                """, unsafe_allow_html=True)
+
+            subrule1_c1, subrule2_c1 = st.columns([1, 1], gap="small")
+            with subrule1_c1:
+                st.markdown("""
+                    <div style='background-color: #34A853; padding: 10px; margin-right: -10px; text-align: center;'>
+                        <p style='color: white; font-size: 14px; margin: 0;'>使用時數:--</p>
+                    </div>
+                    """, unsafe_allow_html=True)
+            with subrule2_c1:
+                st.markdown("""
+                    <div style='background-color: #34A853; padding: 10px; margin-left: -10px; text-align: center;'>
+                        <p style='color: white; font-size: 14px; margin: 0;'>稼動率:--%</p>
+                    </div>
+                    """, unsafe_allow_html=True)
+            
+            mat1_c1, mat2_c1 = st.columns(2)
+            with mat1_c1:
+                st.markdown("""
+                    <div style='background-color: white; padding: 10px; border-radius: 5px;'>
+                        <p style='color: #34A853; font-size: 20px; margin: 0;'>31.60 °C</p>
+                        <p style='color: black; font-size: 16px; margin: 0;'>電池溫度</p>
+                    </div>
+                    """, unsafe_allow_html=True)
+            with mat2_c1:
+                st.markdown("""
+                    <div style='background-color: white; padding: 10px; border-radius: 5px;'>
+                        <p style='color: #34A853; font-size: 20px; margin: 0;'>49.09 V</p>
+                        <p style='color: black; font-size: 16px; margin: 0;'>總電壓</p>
+                    </div>
+                    """, unsafe_allow_html=True)
+            st.markdown("""
+                    <div style='background-color: white; padding: 10px; border-radius: 5px;'>
+                        <p style='color: #34A853; font-size: 20px; margin: 0;'>正常 (狀態)</p>
+                        <p style='color: black; font-size: 16px; margin: 0;'>電池液位</p>
+                    </div>
+                    """, unsafe_allow_html=True)
+
+        with card2:
+            st.markdown("""
+                <div style='background-color: #34A853; padding: 10px; border-radius: 5px 5px 0 0; text-align: center;'>
+                    <p style='color: white; font-size: 32px; margin: 0;'>堆高機004</p>
+                </div>
+                """, unsafe_allow_html=True)
+
+            subrule1_c2, subrule2_c2 = st.columns([1, 1], gap="small")
+            with subrule1_c2:
+                st.markdown("""
+                    <div style='background-color: #34A853; padding: 10px; margin-right: -10px; text-align: center;'>
+                        <p style='color: white; font-size: 14px; margin: 0;'>使用時數:--</p>
+                    </div>
+                    """, unsafe_allow_html=True)
+            with subrule2_c2:
+                st.markdown("""
+                    <div style='background-color: #34A853; padding: 10px; margin-left: -10px; text-align: center;'>
+                        <p style='color: white; font-size: 14px; margin: 0;'>稼動率:--%</p>
+                    </div>
+                    """, unsafe_allow_html=True)
+                
+            mat1_c2, mat2_c2 = st.columns(2)
+            with mat1_c2:
+                st.markdown("""
+                    <div style='background-color: white; padding: 10px; border-radius: 5px;'>
+                        <p style='color: #34A853; font-size: 20px; margin: 0;'>35.40 °C</p>
+                        <p style='color: black; font-size: 16px; margin: 0;'>電池溫度</p>
+                    </div>
+                    """, unsafe_allow_html=True)
+            with mat2_c2:
+                st.markdown("""
+                    <div style='background-color: white; padding: 10px; border-radius: 5px;'>
+                        <p style='color: #34A853; font-size: 20px; margin: 0;'>45.07 V</p>
+                        <p style='color: black; font-size: 16px; margin: 0;'>總電壓</p>
+                    </div>
+                    """, unsafe_allow_html=True)
+            st.markdown("""
+                    <div style='background-color: white; padding: 10px; border-radius: 5px;'>
+                        <p style='color: #34A853; font-size: 20px; margin: 0;'>正常 (狀態)</p>
+                        <p style='color: black; font-size: 16px; margin: 0;'>電池液位</p>
+                    </div>
+                    """, unsafe_allow_html=True)
+            
+    if (f"{option}{''.join(department_options)}" == "客戶服務客戶100"):
+        if on & off:
+            card1, card2 = st.columns(2, gap="small")
+            with card1:
+                st.markdown("""
+                    <div style='background-color: #7F7F7F; padding: 10px; border-radius: 5px 5px 0 0; text-align: center;'>
+                        <p style='color: white; font-size: 32px; margin: 0;'>堆高機C001</p>
+                    </div>
+                    """, unsafe_allow_html=True)
+
+                subrule1_c1, subrule2_c1 = st.columns([1, 1], gap="small")
+                with subrule1_c1:
+                    st.markdown("""
+                        <div style='background-color: #7F7F7F; padding: 10px; margin-right: -10px; text-align: center;'>
+                            <p style='color: white; font-size: 14px; margin: 0;'>使用時數:--</p>
+                        </div>
+                        """, unsafe_allow_html=True)
+                with subrule2_c1:
+                    st.markdown("""
+                        <div style='background-color: #7F7F7F; padding: 10px; margin-left: -10px; text-align: center;'>
+                            <p style='color: white; font-size: 14px; margin: 0;'>稼動率:--%</p>
+                        </div>
+                        """, unsafe_allow_html=True)
+                
+                mat1_c1, mat2_c1 = st.columns(2)
+                with mat1_c1:
+                    st.markdown("""
+                        <div style='background-color: white; padding: 10px; border-radius: 5px;'>
+                            <p style='color: #7F7F7F; font-size: 20px; margin: 0;'>30.50 °C</p>
+                            <p style='color: black; font-size: 16px; margin: 0;'>電池溫度</p>
+                        </div>
+                        """, unsafe_allow_html=True)
+                with mat2_c1:
+                    st.markdown("""
+                        <div style='background-color: white; padding: 10px; border-radius: 5px;'>
+                            <p style='color: #7F7F7F; font-size: 20px; margin: 0;'>49.52 V</p>
+                            <p style='color: black; font-size: 16px; margin: 0;'>總電壓</p>
+                        </div>
+                        """, unsafe_allow_html=True)
+                st.markdown("""
+                        <div style='background-color: white; padding: 10px; border-radius: 5px;'>
+                            <p style='color: #7F7F7F; font-size: 20px; margin: 0;'>正常 (狀態)</p>
+                            <p style='color: black; font-size: 16px; margin: 0;'>電池液位</p>
+                        </div>
+                        """, unsafe_allow_html=True)
+
+            with card2:
+                st.markdown("""
+                    <div style='background-color: #34A853; padding: 10px; border-radius: 5px 5px 0 0; text-align: center;'>
+                        <p style='color: white; font-size: 32px; margin: 0;'>堆高機C002</p>
+                    </div>
+                    """, unsafe_allow_html=True)
+
+                subrule1_c2, subrule2_c2 = st.columns([1, 1], gap="small")
+                with subrule1_c2:
+                    st.markdown("""
+                        <div style='background-color: #34A853; padding: 10px; margin-right: -10px; text-align: center;'>
+                            <p style='color: white; font-size: 14px; margin: 0;'>使用時數:--</p>
+                        </div>
+                        """, unsafe_allow_html=True)
+                with subrule2_c2:
+                    st.markdown("""
+                        <div style='background-color: #34A853; padding: 10px; margin-left: -10px; text-align: center;'>
+                            <p style='color: white; font-size: 14px; margin: 0;'>稼動率:--%</p>
+                        </div>
+                        """, unsafe_allow_html=True)
+                    
+                mat1_c2, mat2_c2 = st.columns(2)
+                with mat1_c2:
+                    st.markdown("""
+                        <div style='background-color: white; padding: 10px; border-radius: 5px;'>
+                            <p style='color: #34A853; font-size: 20px; margin: 0;'>35.40 °C</p>
+                            <p style='color: black; font-size: 16px; margin: 0;'>電池溫度</p>
+                        </div>
+                        """, unsafe_allow_html=True)
+                with mat2_c2:
+                    st.markdown("""
+                        <div style='background-color: white; padding: 10px; border-radius: 5px;'>
+                            <p style='color: #34A853; font-size: 20px; margin: 0;'>45.07 V</p>
+                            <p style='color: black; font-size: 16px; margin: 0;'>總電壓</p>
+                        </div>
+                        """, unsafe_allow_html=True)
+                st.markdown("""
+                        <div style='background-color: white; padding: 10px; border-radius: 5px;'>
+                            <p style='color: #34A853; font-size: 20px; margin: 0;'>正常 (狀態)</p>
+                            <p style='color: black; font-size: 16px; margin: 0;'>電池液位</p>
+                        </div>
+                        """, unsafe_allow_html=True)
+        elif on:
+            st.markdown("""
+                <div style='background-color: #34A853; padding: 10px; border-radius: 5px 5px 0 0; text-align: center;'>
+                    <p style='color: white; font-size: 32px; margin: 0;'>堆高機C002</p>
+                </div>
+                """, unsafe_allow_html=True)
+
+            subrule1_c2, subrule2_c2 = st.columns([1, 1], gap="small")
+            with subrule1_c2:
+                st.markdown("""
+                    <div style='background-color: #34A853; padding: 10px; margin-right: -10px; text-align: center;'>
+                        <p style='color: white; font-size: 14px; margin: 0;'>使用時數:--</p>
+                    </div>
+                    """, unsafe_allow_html=True)
+            with subrule2_c2:
+                st.markdown("""
+                    <div style='background-color: #34A853; padding: 10px; margin-left: -10px; text-align: center;'>
+                        <p style='color: white; font-size: 14px; margin: 0;'>稼動率:--%</p>
+                    </div>
+                    """, unsafe_allow_html=True)
+                
+            mat1_c2, mat2_c2 = st.columns(2)
+            with mat1_c2:
+                st.markdown("""
+                    <div style='background-color: white; padding: 10px; border-radius: 5px;'>
+                        <p style='color: #34A853; font-size: 20px; margin: 0;'>35.40 °C</p>
+                        <p style='color: black; font-size: 16px; margin: 0;'>電池溫度</p>
+                    </div>
+                    """, unsafe_allow_html=True)
+            with mat2_c2:
+                st.markdown("""
+                    <div style='background-color: white; padding: 10px; border-radius: 5px;'>
+                        <p style='color: #34A853; font-size: 20px; margin: 0;'>45.07 V</p>
+                        <p style='color: black; font-size: 16px; margin: 0;'>總電壓</p>
+                    </div>
+                    """, unsafe_allow_html=True)
+            st.markdown("""
+                    <div style='background-color: white; padding: 10px; border-radius: 5px;'>
+                        <p style='color: #34A853; font-size: 20px; margin: 0;'>正常 (狀態)</p>
+                        <p style='color: black; font-size: 16px; margin: 0;'>電池液位</p>
+                    </div>
+                    """, unsafe_allow_html=True)
+        elif off: 
+            st.markdown("""
+                <div style='background-color: #7F7F7F; padding: 10px; border-radius: 5px 5px 0 0; text-align: center;'>
+                    <p style='color: white; font-size: 32px; margin: 0;'>堆高機C001</p>
+                </div>
+                """, unsafe_allow_html=True)
+
+            subrule1_c1, subrule2_c1 = st.columns([1, 1], gap="small")
+            with subrule1_c1:
+                st.markdown("""
+                    <div style='background-color: #7F7F7F; padding: 10px; margin-right: -10px; text-align: center;'>
+                        <p style='color: white; font-size: 14px; margin: 0;'>使用時數:--</p>
+                    </div>
+                    """, unsafe_allow_html=True)
+            with subrule2_c1:
+                st.markdown("""
+                    <div style='background-color: #7F7F7F; padding: 10px; margin-left: -10px; text-align: center;'>
+                        <p style='color: white; font-size: 14px; margin: 0;'>稼動率:--%</p>
+                    </div>
+                    """, unsafe_allow_html=True)
+            
+            mat1_c1, mat2_c1 = st.columns(2)
+            with mat1_c1:
+                st.markdown("""
+                    <div style='background-color: white; padding: 10px; border-radius: 5px;'>
+                        <p style='color: #7F7F7F; font-size: 20px; margin: 0;'>30.50 °C</p>
+                        <p style='color: black; font-size: 16px; margin: 0;'>電池溫度</p>
+                    </div>
+                    """, unsafe_allow_html=True)
+            with mat2_c1:
+                st.markdown("""
+                    <div style='background-color: white; padding: 10px; border-radius: 5px;'>
+                        <p style='color: #7F7F7F; font-size: 20px; margin: 0;'>49.52 V</p>
+                        <p style='color: black; font-size: 16px; margin: 0;'>總電壓</p>
+                    </div>
+                    """, unsafe_allow_html=True)
+            st.markdown("""
+                    <div style='background-color: white; padding: 10px; border-radius: 5px;'>
+                        <p style='color: #7F7F7F; font-size: 20px; margin: 0;'>正常 (狀態)</p>
+                        <p style='color: black; font-size: 16px; margin: 0;'>電池液位</p>
+                    </div>
+                    """, unsafe_allow_html=True)     
+            
+main_content()
--- pages/2監測歷史資料.py
+++ pages/2監測歷史資料.py
@@ -1,6 +1,82 @@
 import streamlit as st
+import datetime
 
-with st.sidebar:
-    st.markdown('''選取時間區間，一鍵輸出歷史報表。''')
+def render_sidebar():
+    with st.sidebar:
+        st.markdown('''選取時間區間，一鍵輸出歷史報表。''')
 
-st.markdown('# 監測歷史資料')
+def render_title():
+    st.title('監測歷史資料')
+    st.markdown("#")
+    st.markdown(f"""
+            <div style='background-color: #19D3F3; padding: 10px; border-radius: 5px;'>
+                <p style='color: black; font-size: 20px; margin: 0;'>查詢頁面</p>
+            </div>
+            """, unsafe_allow_html=True)
+
+def select_part_name():
+    return st.selectbox('部品名稱',
+                        ('堆高機001', '堆高機002', '堆高機003', '堆高機004', '堆高機C001', '堆高機C002'),
+                        index=None)
+
+def date_and_time_range_selection():
+    col1, col2 = st.columns(2)
+    with col1:
+        output = st.radio(
+                "日期類型 👇",
+                ["日期區間", "預先定義的日期"],
+                horizontal=True,
+            )
+        
+        m = st.markdown("""
+        <style>
+        div.stButton > button:first-child {
+            box-shadow: 3px 4px 0px 0px #3e7327;
+            background:linear-gradient(to bottom, #77b55a 5%, #72b352 100%);
+            background-color:#77b55a;
+            border-radius:18px;
+            border:1px solid #4b8f29;
+            display:inline-block;
+            cursor:pointer;
+            color:black;
+            font-family:Arial;
+            font-size:17px;
+            padding:7px 25px;
+            text-decoration:none;
+            text-shadow:0px 1px 0px #5b8a3c;
+        }
+        div.stButton > button:hover {
+            background:linear-gradient(to bottom, #72b352 5%, #77b55a 100%);
+            background-color:#72b352;
+        }
+        div.stButton > button:active {
+            position:relative;
+            top:1px;
+        }
+        </style>""", unsafe_allow_html=True)
+
+        b = st.button("🔍查詢")
+        if b & (part_name != None):
+            st.write("No items to display")
+        elif b & (part_name == None):
+            st.error('請選擇堆高機', icon="🚨")
+        else:
+            st.write("")
+
+    with col2:
+        if output == "日期區間":
+            start_date = st.date_input("開始日期", datetime.date.today())
+            end_date = st.date_input("結束日期", datetime.date.today())
+            start_time = st.time_input("開始時間", datetime.time(8, 0))
+            end_time = st.time_input("結束時間", datetime.time(16, 45))
+            return (start_date, start_time), (end_date, end_time)
+        else:
+            return st.selectbox('日期區間',
+                                ('這個月', '上個月', '上一季', '上半年'),
+                                index=None), None
+
+# Main code execution
+render_sidebar()
+render_title()
+part_name = select_part_name()
+date_time_range = date_and_time_range_selection()
